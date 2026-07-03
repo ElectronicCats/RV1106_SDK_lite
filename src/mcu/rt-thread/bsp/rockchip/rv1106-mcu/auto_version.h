cb9859e2034f39a5b2ce501ec77c7c2d9ca2f07a
Heikki Radú <heikkiperea@gmail.com>
2026-07-03 12:39:23 -0600

    build: stop tracking generated MCU auto_version.h
    auto_version.h is regenerated by the RT-Thread build with a timestamp + git
    hash on every build, so it must not be versioned (it caused spurious diffs).
    Untrack it and add it to .gitignore alongside the other MCU build artifacts.

--- src/mcu/rt-thread/bsp/rockchip/rv1106-mcu/auto_version.h
+++ src/mcu/rt-thread/bsp/rockchip/rv1106-mcu/auto_version.h
@@ -2,6 +2,6 @@
 #define __AUTO_VERSION_H
 
 /* This file is auto generate, please do not edit it. */
-#define FIRMWARE_AUTO_VERSION "2026-07-03 10:54:47.156325 4404679fc(rtt) 4404679fc(hal) 4404679fc(battery-ipc)"
+#define FIRMWARE_AUTO_VERSION "2026-07-03 12:08:26.384542 abf2f4c58(rtt) abf2f4c58(hal) abf2f4c58(battery-ipc)"
 
 #endif
